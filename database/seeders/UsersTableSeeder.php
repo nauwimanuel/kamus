7363bb8b7c42914ca73b2c68f53360d1b19eec34
Imanuel Nauw <imanuelnauw@gmail.com>
2022-04-24 13:58:31 +0900

    menambahkan file BesersTableSeeder dan UsersBeserSeeder dan Ubah DatabaseSeeder

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use illuminate\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = new User;
+        $user->username = "AdminKamus";
+        $user->email = "admin@kamus.com";
+        $user->password = Hash::make('12345678'); 
+        $user->save();
+    }
+}
